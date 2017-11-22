96f1515073d70a51fe60b1299f737bbd23b15250
Daniel Flasch <mail@dflasch.de>
2017-11-22 20:37:05 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var ex1   = require('./routes/ex-1');
+var ex2   = require('./routes/ex-2');
+var ex3   = require('./routes/ex-3');
+var ex4   = require('./routes/ex-4');
+var ex5   = require('./routes/ex-5');
+var ex6   = require('./routes/ex-6');
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/ex-1', ex1);
+app.use('/ex-2', ex2);
+app.use('/ex-3', ex3);
+app.use('/ex-4', ex4);
+app.use('/ex-5', ex5);
+app.use('/ex-6', ex6);
+
+app.use('/assets/jsoneditor', express.static(__dirname + '/node_modules/jsoneditor/dist/'));
+app.use('/assets/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'));
+app.use('/assets/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/ex-2.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('ex-2',
+    { title: 'Aufgabe 2',
+      description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod ' +
+      'tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam ' +
+      'et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum ' +
+      'dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor ' +
+      'invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo ' +
+      'dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
+    });
+});
+
+module.exports = router;
