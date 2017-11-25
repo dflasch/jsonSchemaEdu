f8a801f03b1a6cba728501e45dff53ecfeb95016
Daniel Flasch <mail@dflasch.de>
2017-11-25 18:34:36 +0100

    implemented first exercise

--- routes/ex-1.js
+++ routes/ex-1.js
@@ -1,5 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var jsonFormat = require('json-format');
+var jsonFormatConfig = {
+  type: 'space',
+  size: 2
+};
+
+var schema = {
+  "$schema": "http://json-schema.org/draft-06/schema#",
+  "title": "Product",
+  "description": "A product from Acme's catalog",
+  "type": "object",
+  "properties": {
+    "id": {
+      "description": "The unique identifier for a product",
+      "type": "integer"
+    },
+    "name": {
+      "description": "Name of the product",
+      "type": "string"
+    },
+    "price": {
+      "type": "number",
+      "exclusiveMinimum": 0
+    }
+  },
+  "required": ["id", "name", "price"]
+};
+
+var schemaFormatted = jsonFormat(schema,jsonFormatConfig);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,7 +39,9 @@ router.get('/', function(req, res, next) {
       'et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum ' +
       'dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor ' +
       'invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo ' +
-      'dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
+      'dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+      schema:schema,
+      schemaFormatted:schemaFormatted
     });
 });
 
