7f2fcfdf6e2350e1e79f32e3a06bec4b12ec7850
Daniel Flasch <mail@dflasch.de>
2017-12-05 19:35:44 +0100

    correct exercise 6

--- routes/ex-6.js
+++ routes/ex-6.js
@@ -1,126 +1,201 @@
 var express = require('express');
 var router = express.Router();
-
-var schemaRefs = {
-  "cd":{
-    "type":"object",
-    "properties": {
-      "name": {
-        "type": "object",
-        "properties": {
-          "type": {
-            "type": "string",
-            "enum": ["string"]
-          }
-        },
-        "required":["type"]
-      },
-      "release_year" : {
-        "type":"object",
-        "properties": {
-          "type": {
-            "type": "string",
-            "enum": ["integer", "number"]
-          }
-        },
-        "required":["type"]
-      },
-      "label": {
-        "type":"object",
-        "properties":{
-          "type":{
-            "type":"string",
-            "enum":["string"]
-          }
-        },
-        "required":["type"]
-      }
-    },
-    "required":["name","release_year","label"]
-  },
-  "artist": {
-    "type": "object",
-    "properties": {
-      "name": {
-        "type": "object",
-        "properties":{
-          "type":{
-            "type":"string",
-            "enum": ["string"]
-          }
-        },
-        "required":["type"]
-      },
-
-      "founded_in": {
-        "type":"object",
-        "properties":{
-          "type:":{
-            "type":"string",
-            "enum":["number","integer"]
-          }
-        },
-        "required":["type"]
-      },
-      "biography":{
-        "type":"object",
-        "properties":{
-          "type":{
-            "type:":"string",
-            "enum":["string"]
-          }
-        },
-        "required":["type"]
-      }
-    },
-    "required":["name","founded_in","biography"]
-  },
-  "definitions":{
-    "type":"object",
-    "properties":{
-      "cd":{
-        "$ref":"cd"
-      }
-    },
-    "required":["cd"]
-}
+var jsonFormat = require('json-format');
+var schemaFormatted = [];
+var jsonFormatConfig = {
+  type: 'space',
+  size: 2
 };
 
+var jsonExample = {
+  "name": "Radiohead",
+  "founded_in": 1985,
+  "biography": "Radiohead ist eine britische Alternative-Rock-Band, die 1985 in Oxford, England gegründet wurde. Damals noch ....",
+  "cds": [
+    {
+      "name": "OK Computer",
+      "release_year": 1997,
+      "label": "Parlophone/EMI (UK); Capitol (USA)"
+    }
+  ]
+};
+
+schemaFormatted.push(jsonFormat(jsonExample, jsonFormatConfig));
+
+var schemaRefs = {
+    "cd": {
+      "type": "object",
+      "properties": {
+        "type": {
+          "type": "string",
+          "enum": ["object"]
+        },
+
+        "properties": {
+          "type": "object",
+          "properties": {
+            "name": {
+              "type": "object",
+              "properties": {
+                "type": {
+                  "type": "string",
+                  "enum": ["string"]
+                }
+              },
+              "required": ["type"]
+            },
+            "release_year": {
+              "type": "object",
+              "properties": {
+                "type": {
+                  "type": "string",
+                  "enum": ["integer", "number"]
+                }
+              },
+              "required": ["type"]
+            },
+            "label": {
+              "type": "object",
+              "properties": {
+                "type": {
+                  "type": "string",
+                  "enum": ["string"]
+                }
+              },
+              "required": ["type"]
+            }
+          },
+          "required": ["name"]
+        }
+
+      },
+      "required": ["type", "properties"]
+    },
+    "artist":
+      {
+        "type":
+          "object",
+        "properties":
+          {
+            "name":
+              {
+                "type":
+                  "object",
+                "properties":
+                  {
+                    "type":
+                      {
+                        "type":
+                          "string",
+                        "enum":
+                          ["string"]
+                      }
+                  }
+                ,
+                "required":
+                  ["type"]
+              }
+            ,
+
+            "founded_in":
+              {
+                "type":
+                  "object",
+                "properties":
+                  {
+                    "type:":
+                      {
+                        "type":
+                          "string",
+                        "enum":
+                          ["number", "integer"]
+                      }
+                  }
+                ,
+                "required":
+                  ["type"]
+              }
+            ,
+            "biography":
+              {
+                "type":
+                  "object",
+                "properties":
+                  {
+                    "type":
+                      {
+                        "type:":
+                          "string",
+                        "enum":
+                          ["string"]
+                      }
+                  }
+                ,
+                "required":
+                  ["type"]
+              }
+          }
+        ,
+        "required":
+          ["name", "founded_in", "biography"]
+      }
+    ,
+    "definitions":
+      {
+        "type":
+          "object",
+        "properties":
+          {
+            "cd":
+              {
+                "$ref":
+                  "cd"
+              }
+          }
+        ,
+        "required":
+          ["cd"]
+      }
+  }
+;
+
 var schema = {
-  "type":"object",
-  "properties" : {
+  "type": "object",
+  "properties": {
     "$schema": {
-      "type":"string"
+      "type": "string"
     },
-    "id":{
-      "type":"string"
+    "id": {
+      "type": "string"
     },
-    "title":{
-      "type":"string"
+    "title": {
+      "type": "string"
     },
-    "type":{
-      "type":"string"
+    "type": {
+      "type": "string"
     },
-    "definitions":{
+    "definitions": {
       "$ref": "definitions"
     },
-    "properties": { "$ref": "artist" }
+    "properties": {"$ref": "artist"}
   },
-  "required":["$schema","$id","title","type","definitions","properties"]
+  "required": ["$schema", "$id", "title", "type", "definitions", "properties"]
 };
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('ex-6',
-    { title: 'Aufgabe 6',
+    {
+      title: 'Aufgabe 6',
       description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod ' +
       'tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam ' +
       'et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum ' +
       'dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor ' +
       'invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo ' +
       'dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-      schema:schema,
-      schemaRefs:schemaRefs
+      schema: schema,
+      schemaRefs: schemaRefs,
+      schemaFormatted: schemaFormatted
     });
 });
 
